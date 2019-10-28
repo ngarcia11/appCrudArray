9f8cdd2006c99bfdb999bbc3b99432615c5a2e15
ngarcia11 <49655787+ngarcia11@users.noreply.github.com>
2019-10-27 21:16:29 -0600

    Tres acciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-crud-array';
+  title = 'Norlan';
+  users  : any = [{
+    id : 700093,   
+    name : 'Norlan Garcia',
+    address : 'Colonia Kennedy'
+    }];
+
+  constructor() {
+    
+  
+  }
+
+  selectedUser: any = {id : '', name: '', address: ''};
+
+  openForEdit(user: any, btn): void 
+  {
+    this.selectedUser = user;
+    btn.value = "Modificar";
+  }
+
+
+  addUser(newid, newname, newaddress){
+    if(this.selectedUser.id === 0) 
+    {
+    this.users.push({id : newid.value, name: newname.value, address: newaddress.value});
+    newname.value = "";
+    newid.value = "";
+    newaddress.value = "";
+    }
+    this.selectedUser = {id : '', name: '', address: ''};
+    return false;
+  }
+    deleteUser(user) {
+      if(confirm('Estas seguro de eliminar el usuario?'))
+      {
+      for(let i=0; i < this.users.length; i ++){
+        if (user == this.users[i]){
+          this.users.splice(i,1);
+        }
+      }
+    }
+      }
 }
